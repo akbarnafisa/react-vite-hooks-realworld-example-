d95029a59ce90afbb318ee69289ab8f6e006958e
Akbar Nafisa <akbarnj788@gmail.com>
2023-08-14 10:49:03 +0700

    feat: add Form erros component

---
+++ src/components/FormErrors.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useFormikContext } from 'formik'
+import { isEmpty } from 'lodash-es'
+
+function FormErrors() {
+  const { errors } = useFormikContext()
+
+  if (isEmpty(errors)) return null
+
+  if (Array)
+
+  return (
+    <ul className="error-messages">
+      {Object.entries(errors).map(([key, messages]) =>
+        /** @type {string[]} */ (messages).map((message) => (
+          <li key={`${key} ${message}`}>
+            {key} {message}
+          </li>
+        ))
+      )}
+    </ul>
+  )
+}
+
+export default FormErrors

b632733bd254de866a454371af3ad8f5b5fed746
Akbar Nafisa <akbarnj788@gmail.com>
2023-08-14 09:46:17 +0700

    feat: add useAuth hooks

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,49 @@
+import axios from 'axios'
+import { isEmpty } from 'lodash-es'
+import { useSnapshot, proxy } from 'valtio'
+
+function getAuthUser() {
+  const jwt = window.localStorage.getItem('jwtToken')
+
+  if (!jwt) {
+    return {}
+  }
+
+  return JSON.parse(atob(jwt))
+}
+
+const state = proxy({
+  authUser: getAuthUser(),
+  get isAuth() {
+    return !isEmpty(state.authUser)
+  }
+})
+
+const actions = {
+  login: (user) => {
+    state.authUser = user
+    window.localStorage.setItem('jwtToken', btoa(JSON.stringify(state.authUser)))
+    axios.defaults.headers.Authorization = `Token ${state.authUser.token}`
+  },
+  logout: () => {
+    state.authUser = {}
+    window.localStorage.removeItem('jwtToken')
+  },
+  checkAuth: () => {
+    const authUser = getAuthUser()
+
+    if (!authUser || isEmpty(authUser)) {
+      actions.logout()
+    }
+  }
+}
+
+function useAuth() {
+  const snap = useSnapshot(state)
+  return {
+    ...snap,
+    ...actions,
+  }
+}
+
+export default useAuth
